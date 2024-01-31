21330f520300086f32d5e31faadb4ecc070d0896
Basil Nelson <basil.nelson@usu.edu>
2024-01-31 13:34:12 -0700

    mostly complete pseudocode for toString()

--- Tree.java
+++ Tree.java
@@ -43,7 +43,24 @@ public class Tree<E extends Comparable<? super E>> {
      * Return a string containing the tree contents as a tree with one node per line
      */
     public String toString() {
-        // TODO:
+        // TODO: Figure out indent/formatting
+        // TODO: Make sure self returns before left branches, but left branches print after
+
+        // while namePrinted false
+            // print name
+            // set namePrinted to true
+
+        // find parent node value
+        // if no parent node, save as "no parent"
+
+        // if tree empty, print "empty tree"
+
+        // if node has right child
+            // call toString for right child
+        // if node has left child
+            // call toString for left child
+
+        // return "node value, [parent node value]")
         return "";
     }
 
@@ -52,6 +69,10 @@ public class Tree<E extends Comparable<? super E>> {
      */
     public String inOrderToString() {
         // TODO:
+
+        //
+
+
         return "";
     }
 
